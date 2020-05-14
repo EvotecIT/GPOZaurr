3ff20afe30d4c1254654d9328ff200b320c66b1e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-05-14 13:26:27 +0200

    Updated examples

---
+++ Examples/Example-17-RepairInconsistentACL.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
+
+Get-GPOZaurrPermissionConsistency -GPOName 'Default Domain Controllers Policy' -IncludeDomains 'ad.evotec.xyz'
+
+Repair-GPOZaurrPermissionConsistency -GPOName 'Default Domain Controllers Policy' -IncludeDomains 'ad.evotec.xyz' #-WhatIf -Verbose
