4acf509e2b9928a10c4bff7d2dd7195ef368f63c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-05-26 19:16:32 +0200

    Update

--- Examples/Example-20-RemoveOrphanedFoldersSysvol.ps1
+++ Examples/Example-20-RemoveOrphanedFoldersSysvol.ps1
@@ -1,3 +1,7 @@
 ﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
 
+# this allows you to process X amount of orphaned folders/files (good for testing)
+Remove-GPOZaurrOrphanedSysvolFolders -Verbose -WhatIf -LimitProcessing 2
+
+# this runs for whole SYSVOL and checks things against GPOS
 Remove-GPOZaurrOrphanedSysvolFolders -Verbose -WhatIf
