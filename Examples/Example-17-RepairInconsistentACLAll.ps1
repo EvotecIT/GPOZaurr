4e2ff766613205d250f381fda654bfbe5ff1f4ab
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-05-29 13:07:41 +0200

    Example

---
+++ Examples/Example-17-RepairInconsistentACLAll.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
+
+Get-GPOZaurrPermissionConsistency -Type All | Format-Table
+
+Repair-GPOZaurrPermissionConsistency -LimitProcessing 2 -WhatIf -Verbose
