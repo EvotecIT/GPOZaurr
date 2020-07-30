d3c31e965181f740819d15ef9c5e147b85128fd6
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-07-30 21:58:41 +0200

    Update example

---
+++ Examples/Example-29-FindGPO2.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
+
+# This gets the same thing as earlier examples
+# with a difference where one entry per gpo and all settings for that GPO is stored under settings property.
+$Output = Invoke-GPOZaurr -SingleObject
+$Output | Format-Table
+$Output.Reports.RegistrySettings | Format-Table *
+$Output.Reports.RegistrySettings[0].Settings | Format-Table *
