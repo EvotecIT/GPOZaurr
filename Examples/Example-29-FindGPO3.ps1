34c55c8482b2b68e7aa214e6090bfa158688210d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-02 00:31:20 +0200

    Example update

--- Examples/Example-29-FindGPO3.ps1
+++ Examples/Example-29-FindGPO3.ps1
@@ -1,9 +1,13 @@
 ﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
 
 # Use Save-GPOZaurrFiles -GPOPath $ENV:USERPROFILE\Desktop\GPOExportAudit
-$Output = Invoke-GPOZaurr -GPOPath $ENV:USERPROFILE\Desktop\GPOExportAudit # -NoTranslation
+$Output = Invoke-GPOZaurr -GPOPath $ENV:USERPROFILE\Desktop\GPOExportAudit -Verbose #-SkipCleanup #-Type PoliciesPrinters, Policies
 $Output | Format-Table *
 
 $Output.Reports | Format-Table
-#$Output.Reports.LAPS | Format-Table *
-$Output.Reports.LithnetFilter | Format-List *
+
+# Export to Excel
+foreach ($Key in $Output.Reports.Keys) {
+    $Output.Reports[$Key] | ConvertTo-Excel -FilePath $Env:USERPROFILE\Desktop\GPOAnalysis.xlsx -ExcelWorkSheetName $Key -AutoFilter -AutoFit -FreezeTopRowFirstColumn
+}
+Start-Process "$Env:USERPROFILE\Desktop\GPOAnalysis.xlsx"
