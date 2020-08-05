40e422682b1fca9a1befc7536b4ae730cb176f07
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-05 13:58:56 +0200

    New command Get-GPOZaurrDictionary

---
+++ Public/Get-GpoZaurrDictionary.ps1
@@ -0,0 +1,26 @@
+﻿function Get-GPOZaurrDictionary {
+    [cmdletBinding()]
+    param(
+        [string] $Splitter = [System.Environment]::NewLine
+    )
+    foreach ($Policy in $Script:GPODitionary.Keys) {
+
+        if ($Script:GPODitionary[$Policy].ByReports) {
+            [Array] $Type = foreach ($T in  $Script:GPODitionary[$Policy].ByReports ) {
+                $T.Report
+            }
+
+        } else {
+            [Array]$Type = foreach ($T in $Script:GPODitionary[$Policy].Types) {
+                ( -join ($T.Category, '/', $T.Settings))
+            }
+        }
+
+        [PSCustomObject] @{
+            Name  = $Policy
+            Types = $Type -join $Splitter
+            Path  = $Script:GPODitionary[$Policy].GPOPath -join $Splitter
+            #Details = $Script:GPODitionary[$Policy]
+        }
+    }
+}
