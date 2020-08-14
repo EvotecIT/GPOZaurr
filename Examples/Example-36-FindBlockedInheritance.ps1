0c26642a72fe12eee7e0fe11ef7c9dc500019bc0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-14 23:00:54 +0200

    Update example

---
+++ Examples/Example-36-FindBlockedInheritance.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
+
+# Get basic output DN, CanonicalName, BlockInheritance (True/False)
+#$Objects = Get-GPOZaurrInheritance
+#$Objects | Format-Table
+
+# Get same output DN, CanonicalName, BlockInheritance (True/False) + Users/Computers + UsersCount/ComputerCount for those with Blocked Inhertiance
+# This is so you can have a list what machines are affected
+$Objects = Get-GPOZaurrInheritance -IncludeBlockedObjects -OnlyBlockedInheritance
+$Objects | Format-Table
