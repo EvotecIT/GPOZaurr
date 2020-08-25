fdf78ecadb4f55094a7fca9058f250c250df7f7e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-25 12:37:03 +0200

    Added Get-GPOZaurrPermissionSummary report

---
+++ Public/Get-GPOZaurrPermissionSummary.ps1
@@ -0,0 +1,34 @@
+﻿function Get-GPOZaurrPermissionSummary {
+    [cmdletBinding()]
+    param(
+        [Microsoft.GroupPolicy.GPPermissionType[]] $IncludePermissionType,
+        [Microsoft.GroupPolicy.GPPermissionType[]] $ExcludePermissionType,
+
+        [alias('ForestName')][string] $Forest,
+        [string[]] $ExcludeDomains,
+        [alias('Domain', 'Domains')][string[]] $IncludeDomains,
+        [System.Collections.IDictionary] $ExtendedForestInformation
+    )
+
+    $Permissions = Get-GPOZaurrPermission -Forest $Forest -IncludeDomains $IncludeDomains -ExcludeDomains $ExcludeDomains -ExtendedForestInformation $ExtendedForestInformation -IncludePermissionType $IncludePermissionType -ExcludePermissionType $ExcludePermissionType
+    $Entries = foreach ($Permission in $Permissions) {
+        [PSCustomObject] @{
+            Name        = $Permission.Name
+            Permission  = $Permission.Permission
+            Sid         = $Permission.Sid
+            SidType     = $Permission.SidType
+            DisplayName = $Permission.DisplayName
+            DomainName  = $Permission.DomainName
+            Domain      = $Domain
+        }
+    }
+    $Entries | Group-Object -Property Permission, Name, DomainName | ForEach-Object {
+        $Property = $_.Name -split ', '
+        [PSCustomObject] @{
+            Permission = $Property[0]
+            Name       = $Property[1]
+            DomainName = $Property[2]
+            GPOCount   = $_.Count
+        }
+    }
+}
