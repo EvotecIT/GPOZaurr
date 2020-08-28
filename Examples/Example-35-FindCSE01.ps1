a1d83930602b084241322dd724aaa0288109375d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-28 08:28:53 +0200

    Updated examples

---
+++ Examples/Example-35-FindCSE01.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
+
+ConvertFrom-CSExtension -CSE '{FC715823-C5FB-11D1-9EEF-00A0C90347FF}'
+ConvertFrom-CSExtension -CSE '{A2E30F80-D7DE-11d2-BBDE-00C04F86AE3B}'
