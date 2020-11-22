3ab54ec4ad6cf389dd2b0569ccc79b1d490a0e6f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-11-22 18:48:30 +0100

    Update example 2

--- Examples/Example-20-RemoveOrphanedFoldersSysvol.ps1
+++ Examples/Example-20-RemoveOrphanedFoldersSysvol.ps1
@@ -4,7 +4,7 @@
 Get-GPOZaurrBroken -Verbose -IncludeDomains 'ad.evotec.pl' | Format-Table
 
 # this allows you to process X amount of orphaned folders/files (good for testing)
-Remove-GPOZaurrBroken -Verbose -WhatIf -IncludeDomains 'ad.evotec.pl' #-LimitProcessing 2
+Remove-GPOZaurrBroken -Verbose -WhatIf #-IncludeDomains 'ad.evotec.pl' #-LimitProcessing 2
 
 # this runs for whole SYSVOL and checks things against GPOS
 Remove-GPOZaurrBroken -Verbose -IncludeDomains 'ad.evotec.xyz' -BackupPath $Env:UserProfile\Desktop\MyBackup1 -WhatIf
