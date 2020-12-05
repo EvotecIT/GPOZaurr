1208f0af7fc958418c07bcaaf083e435337d7972
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-12-05 23:56:05 +0100

    Update examples

--- Examples/Example-43-OptimizeGPO.ps1
+++ Examples/Example-43-OptimizeGPO.ps1
@@ -1,3 +1,8 @@
 ﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
 
-Optimize-GPOZaurr -All -WhatIf -Verbose -LimitProcessing 2
+# Optimize All
+Optimize-GPOZaurr -All -Verbose -IncludeDomains 'ad.evotec.pl' -LimitProcessing 1 {
+    Skip-GroupPolicy -Name 'TEST | Empty GPO - AD.EVOTEC.PL CrossDomain GPO' -DomaiName 'ad.evotec.pl'
+}
+# Optimize just one
+#Optimize-GPOZaurr -GPOName 'TEST | Empty GPO - AD.EVOTEC.PL CrossDomain GPO' -WhatIf -Verbose
