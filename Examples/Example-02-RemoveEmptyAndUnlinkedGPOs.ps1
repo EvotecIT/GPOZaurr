6c2818121d576a213a2ba26e3404340317761dda
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-12-06 10:15:55 +0100

    Update example

--- Examples/Example-02-RemoveEmptyAndUnlinkedGPOs.ps1
+++ Examples/Example-02-RemoveEmptyAndUnlinkedGPOs.ps1
@@ -2,3 +2,9 @@
 
 # Remove GPOS
 Remove-GPOZaurr -Type Empty, Unlinked -BackupPath "$Env:UserProfile\Desktop\GPO" -BackupDated -LimitProcessing 2 -Verbose -WhatIf
+
+# Remove GPOS, but don't touch 2 defined exclusions
+Remove-GPOZaurr -Type Empty, Unlinked -BackupPath "$Env:UserProfile\Desktop\GPO" -BackupDated -LimitProcessing 2 -Verbose -WhatIf {
+    Skip-GroupPolicy -Name 'TEST | Drive Mapping 1'
+    Skip-GroupPolicy -Name 'TEST | Drive Mapping 2' -DomaiName 'ad.evotec.pl'
+}
