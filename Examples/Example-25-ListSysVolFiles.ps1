4d050c536de773b327e51eb85ab9c16991275be5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-12-12 22:15:07 +0100

    Update examples

--- Examples/Example-25-ListSysVolFiles.ps1
+++ Examples/Example-25-ListSysVolFiles.ps1
@@ -2,5 +2,5 @@
 Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
 
 Get-GPOZaurrFiles -Type All | Out-HtmlView -ScrollX -Filtering -AllProperties
-Get-GPOZaurrFiles -HashAlgorithm MD5 | Select-Object -First 2 | ConvertTo-Excel -FilePath $Env:USERPROFILE\Desktop\GPOListFiles.xlsx -AllProperties -ExcelWorkSheetName 'Files Just 2' -AutoFilter -AutoFit #-OpenWorkBook
-Get-GPOZaurrFiles -Type All -HashAlgorithm SHA256 | ConvertTo-Excel -FilePath $Env:USERPROFILE\Desktop\GPOListFiles.xlsx -AllProperties -ExcelWorkSheetName 'Files All' -AutoFilter -AutoFit #-OpenWorkBook
+#Get-GPOZaurrFiles -HashAlgorithm MD5 | Select-Object -First 2 | ConvertTo-Excel -FilePath $Env:USERPROFILE\Desktop\GPOListFiles.xlsx -AllProperties -ExcelWorkSheetName 'Files Just 2' -AutoFilter -AutoFit #-OpenWorkBook
+#Get-GPOZaurrFiles -Type All -HashAlgorithm SHA256 | ConvertTo-Excel -FilePath $Env:USERPROFILE\Desktop\GPOListFiles.xlsx -AllProperties -ExcelWorkSheetName 'Files All' -AutoFilter -AutoFit #-OpenWorkBook
