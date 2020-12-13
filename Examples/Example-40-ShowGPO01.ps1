49b9eccfd16ca209270c7ae3d54d0028ab1bca34
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-12-13 14:54:27 +0100

    Update Example

--- Examples/Example-40-ShowGPO01.ps1
+++ Examples/Example-40-ShowGPO01.ps1
@@ -1,14 +1,4 @@
 ﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
 
-# GPOConsistency,GPODuplicates,GPOOrphans,GPOOwners,NetLogonOwners,GPOPermissionsRead,GPOPermissionsAdministrative - functional
-
 #$Output = Invoke-GPOZaurr -FilePath $PSScriptRoot\Reports\GPOZaurr.html -PassThru -Type GPOConsistency, GPOList, GPODuplicates, GPOOrphans, GPOOwners, NetLogonOwners, GPOPermissionsRead, GPOPermissionsAdministrative,GPOPermissionsUnknown
 Invoke-GPOZaurr -FilePath $PSScriptRoot\Reports\GPOZaurr.html -Type GPOPermissions
-
-return
-#
-$GPOS = Get-GPOZaurr -GPOPath 'C:\Support\GitHub\GpoZaurr\Ignore\Empty' -ExcludeGroupPolicies @(
-    Skip-GroupPolicy -Name 'de14_usr_std'
-    Skip-GroupPolicy -Name 'de14_usr_std' -DomaiName 'ad.evotec.xyz'
-)
-$GPOS | Format-Table -AutoSize *
