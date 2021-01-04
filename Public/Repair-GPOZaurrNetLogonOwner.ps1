d094344f9ffe9efc0155a6e69cd450a5aeb9a02c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-01-04 21:15:28 +0100

    Update docs

--- Public/Repair-GPOZaurrNetLogonOwner.ps1
+++ Public/Repair-GPOZaurrNetLogonOwner.ps1
@@ -1,4 +1,35 @@
 ﻿function Repair-GPOZaurrNetLogonOwner {
+    <#
+    .SYNOPSIS
+    Sets new owner to each file in NetLogon share.
+
+    .DESCRIPTION
+    Sets new owner to each file in NetLogon share.
+
+    .PARAMETER Forest
+    Target different Forest, by default current forest is used
+
+    .PARAMETER ExcludeDomains
+    Exclude domain from search, by default whole forest is scanned
+
+    .PARAMETER IncludeDomains
+    Include only specific domains, by default whole forest is scanned
+
+    .PARAMETER ExtendedForestInformation
+    Ability to provide Forest Information from another command to speed up processing
+
+    .PARAMETER Principal
+    Provide named owner. If not provided default S-1-5-32-544 is used.
+
+    .PARAMETER LimitProcessing
+    Allows to specify maximum number of items that will be fixed in a single run. It doesn't affect amount of GPOs processed
+
+    .EXAMPLE
+    Repair-GPOZaurrNetLogonOwner -WhatIf -Verbose -IncludeDomains ad.evotec.pl
+
+    .NOTES
+    General notes
+    #>
     [cmdletBinding(SupportsShouldProcess)]
     param(
         [alias('ForestName')][string] $Forest,
