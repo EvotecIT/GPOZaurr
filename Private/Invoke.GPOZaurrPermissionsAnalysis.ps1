a68d917d1955d433e2af4323b988ec9bf5e1c273
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-01-11 09:51:10 +0100

    Small test

--- Private/Invoke.GPOZaurrPermissionsAnalysis.ps1
+++ Private/Invoke.GPOZaurrPermissionsAnalysis.ps1
@@ -279,7 +279,14 @@
             foreach ($Domain in $Script:Reporting['GPOPermissions']['Variables']['WillFixUnknownPerDomain'].Keys) {
                 New-HTMLListItem -Text "$Domain requires ", $Script:Reporting['GPOPermissions']['Variables']['WillFixUnknownPerDomain'][$Domain], " changes." -FontWeight normal, bold, normal
             }
+        } -FontSize 10pt -LineBreak
+        # Just in case report is too big and the full file is not attached
+        New-HTMLText -FontSize 10pt -Text "To generate up to date report please execute: " -FontWeight bold
+        New-HTMLList -Type Unordered {
+            New-HTMLListItem -Text 'Install-Module GPOZaurr -Force', ' or ', ' install module manually.' -FontWeight bold, normal, bold
+            New-HTMLListItem -Text 'Invoke-GPOZaurr -FilePath $Env:UserProfile\Desktop\GPOZaurrGPOPermissionsBefore.html -Verbose -Type GPOPermissions' -FontWeight bold
         } -FontSize 10pt
+        New-HTMLText -FontSize 10pt -Text 'Steps above will generate above summary with more details allowing you to get up to date report and steps on how to fix it.'
     }
     Solution   = {
         New-HTMLSection -Invisible {
