53aa69029fd7225481ece93b891b7e784ad4b2d0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-11 21:11:19 +0200

    Update examples

--- Examples/Example-40-ShowGPO05.ps1
+++ Examples/Example-40-ShowGPO05.ps1
@@ -1,7 +1,7 @@
 ﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
 
-$GPOS = Get-GPOZaurr -GPOPath 'C:\Support\GitHub\GpoZaurr\Ignore\Empty' -ExcludeGroupPolicies @(
+$GPOS = Get-GPOZaurr -GPOPath 'C:\Support\GitHub\GpoZaurr\Ignore\Empty' -ExcludeGroupPolicies {
     Skip-GroupPolicy -Name 'de14_usr_std'
     Skip-GroupPolicy -Name 'de14_usr_std' -DomaiName 'ad.evotec.xyz'
-)
+}
 $GPOS | Format-Table -AutoSize *
