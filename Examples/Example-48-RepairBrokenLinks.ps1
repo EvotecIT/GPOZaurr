2ef3c306ff77d756fc48172115cff2ef3cc9cd6a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-11 14:53:37 +0200

    Update example

--- Examples/Example-48-RepairBrokenLinks.ps1
+++ Examples/Example-48-RepairBrokenLinks.ps1
@@ -1,5 +1,5 @@
 Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
 
-Repair-GPOZaurrBrokenLink -Verbose -LimitProcessing 1 -WhatIf
+Repair-GPOZaurrBrokenLink -Verbose -LimitProcessing 1 #-WhatIf
 
 #Repair-GPOZaurrBrokenLink -Verbose -IncludeDomains ad.evotec.pl -LimitProcessing 30 #-WhatIf
