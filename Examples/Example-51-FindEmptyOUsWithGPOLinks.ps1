0dc56d314ed4a26264aa4aa29cb22eb2f1c5c799
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-10 16:11:17 +0200

    Example improvement

--- Examples/Example-51-FindEmptyOUsWithGPOLinks.ps1
+++ Examples/Example-51-FindEmptyOUsWithGPOLinks.ps1
@@ -1,5 +1,16 @@
 ﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
 
-#Get-GPOZaurrOrganizationalUnit -Verbose -Option Unlink | Format-Table
+#Get-GPOZaurrOrganizationalUnit -Verbose -Option Unlink -Exclusions @(
+#    'OU=Groups,OU=Production,DC=ad,DC=evotec,DC=pl'
+#) | Format-Table
 
-Invoke-GPOZaurr -Type GPOOrganizationalUnit -Online -FilePath $PSScriptRoot\Reports\GPOZaurrOU.html
+#Get-GPOZaurrOrganizationalUnit -Verbose -ExcludeOrganizationalUnit @(
+#    '*,OU=Production,DC=ad,DC=evotec,DC=pl'
+#) | Format-Table
+
+
+Invoke-GPOZaurr -Type GPOOrganizationalUnit -Online -FilePath $PSScriptRoot\Reports\GPOZaurrOU.html -Exclusions @(
+    '*OU=Production,DC=ad,DC=evotec,DC=pl'
+    '*OU=Production,DC=ad,DC=evotec,DC=pl'
+    '*OU=Production,DC=ad,DC=evotec,DC=pl'
+)
