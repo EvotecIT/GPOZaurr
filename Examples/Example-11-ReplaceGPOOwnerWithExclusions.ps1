5a2f7893210e7c33c75234215be0a2efe9c09c7a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-10 20:57:38 +0200

    Update example showing exclusions

---
+++ Examples/Example-11-ReplaceGPOOwnerWithExclusions.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
+
+$GPOs = Get-GPOZaurrOwner -IncludeSysvol -Verbose
+$GPOs | Format-Table DisplayName, Status, Owner, OwnerSID, OwnerType, SysvolOwner, SysvolSID, SysvolType
+
+#Set-GPOZaurrOwner -Type All -Verbose -LimitProcessing 2 -WhatIf -IncludeDomains 'ad.evotec.xyz'
+
+Set-GPOZaurrOwner -Type All -Verbose -LimitProcessing 2 -WhatIf -IncludeDomains 'ad.evotec.xyz' -ApprovedOwner @(
+    'EVOTEC\przemyslaw.klys'
+)
