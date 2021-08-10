ff1668c60d43e4c2d2789da0797eee2b48213fa5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-10 16:11:51 +0200

    Function that generates exclusions

---
+++ Private/New-GPOZaurrExclusions.ps1
@@ -0,0 +1,24 @@
+﻿function New-GPOZaurrExclusions {
+    [cmdletBinding()]
+    param(
+        [Array] $ExclusionsArray,
+        [ScriptBlock] $ExclusionsScriptBlock
+    )
+
+    if ($ExclusionsArray) {
+        [string] $Code = @(
+            '$Exclusions = @('
+            [System.Environment]::NewLine
+            foreach ($Exclusion in $ExclusionsArray) {
+                "   `"$Exclusion`"" + [System.Environment]::NewLine
+            }
+            [System.Environment]::NewLine
+            ')'
+        )
+        $Code
+    } elseif ( $ExclusionsScriptBlock ) {
+        throw "ExclusionsScriptBlock is not supported yet"
+    } else {
+        throw "ExclusionsArray or ExclusionsScriptBlock must be specified"
+    }
+}
