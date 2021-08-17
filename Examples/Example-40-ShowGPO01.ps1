bb77785ea8a3feaf951f459bfbfb055d55362286
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-17 10:20:30 +0200

    Update example

--- Examples/Example-40-ShowGPO01.ps1
+++ Examples/Example-40-ShowGPO01.ps1
@@ -1,16 +1,21 @@
 ﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
 
-#$Output = Invoke-GPOZaurr -FilePath $PSScriptRoot\Reports\GPOZaurr.html -PassThru -Type GPOConsistency, GPOList, GPODuplicates, GPOBroken, GPOOwners, NetLogonOwners, GPOPermissionsRead, GPOPermissionsAdministrative,GPOPermissionsUnknown
+Invoke-GPOZaurr -Type GPOOrganizationalUnit -Online -FilePath $PSScriptRoot\Reports\GPOZaurr.html
+
+return
 
 # Shows how to use exclusions (supported only in GPOBlockedInheritance)
 Invoke-GPOZaurr -FilePath $PSScriptRoot\Reports\GPOZaurr.html -Type GPOBlockedInheritance -Online -Exclusions @(
     'OU=Test,OU=ITR02,DC=ad,DC=evotec,DC=xyz'
 )
 
-<#
+# different approach to query multiple reports or just one
+Invoke-GPOZaurr -FilePath $PSScriptRoot\Reports\GPOZaurr.html -PassThru -Type GPOConsistency, GPOList, GPODuplicates, GPOBroken, GPOOwners, NetLogonOwners, GPOPermissionsRead, GPOPermissionsAdministrative,GPOPermissionsUnknown
+
+Invoke-GPOZaurr -Type GPOOwners -Online -FilePath $PSScriptRoot\Reports\GPOZaurr.html
+
 # Shows how to use exclusions for GPOList (different way)
 Invoke-GPOZaurr -FilePath $PSScriptRoot\Reports\GPOZaurr.html -Type GPOList -Online -Exclusions {
     Skip-GroupPolicy -Name 'de14_usr_std'
     Skip-GroupPolicy -Name 'ALL | Enable RDP' -DomaiName 'ad.evotec.xyz'
 }
-#>
