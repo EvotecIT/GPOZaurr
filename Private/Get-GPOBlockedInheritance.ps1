b6fc7a676b0c5b4cdd15b22b2b2e0f642c50f1ef
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-10-17 14:55:59 +0200

    Internal function

---
+++ Private/Get-GPOBlockedInheritance.ps1
@@ -0,0 +1,30 @@
+﻿function Get-GPOBlockedInheritance {
+    [cmdletBinding()]
+    param(
+        [string] $Filter = '*',
+
+        [alias('ForestName')][string] $Forest,
+        [string[]] $ExcludeDomains,
+        [alias('Domain', 'Domains')][string[]] $IncludeDomains,
+        [switch] $AsHashTable,
+        [System.Collections.IDictionary] $ExtendedForestInformation
+    )
+    $OUCache = [ordered] @{}
+
+    $ForestInformation = Get-WinADForestDetails -Extended -Forest $Forest -IncludeDomains $IncludeDomains -ExcludeDomains $ExcludeDomains -ExtendedForestInformation $ExtendedForestInformation
+
+    foreach ($Domain in $ForestInformation.Domains) {
+        $OrganizationalUnits = Get-ADOrganizationalUnit -Filter $Filter -Properties gpOptions, canonicalName -Server $ForestInformation['QueryServers'][$Domain]['HostName'][0] #-SearchScope Subtree
+        foreach ($OU in $OrganizationalUnits) {
+            $OUCache[$OU.DistinguishedName] = [PSCustomObject] @{
+                DistinguishedName  = $OU.DistinguishedName
+                BlockedInheritance = if ($OU.gpOptions -eq 1) { $true } else { $false } # blocked inheritance
+            }
+        }
+    }
+    if ($AsHashTable) {
+        $OUCache
+    } else {
+        $OUCache.Values
+    }
+}
