4985a114dc7b9934e0e08aeb391ace8085013563
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-10-22 12:16:39 +0200

    Added example

---
+++ Examples/Example-52-GetNewGPOs.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
+
+Get-GPOZaurrUpdates -DateRange Last14Days -DateProperty WhenCreated, WhenChanged -Verbose -IncludeDomains 'ad.evotec.pl' | Format-List
+Get-GPOZaurrUpdates -DateRange Last14Days -DateProperty WhenCreated -Verbose | Format-Table
