1a54d62b2eb46d61de29010b9aab1333c211b806
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-10-24 15:46:20 +0200

    Fix example

--- Examples/Example-40-ShowGPO05.ps1
+++ Examples/Example-40-ShowGPO05.ps1
@@ -9,8 +9,14 @@ $GPOS = Get-GPOZaurr -ExcludeGroupPolicies {
 }
 $GPOS | Format-Table -AutoSize *
 
-Invoke-GPOZaurr -Type GPOList -Exclusions {
+$Output = Invoke-GPOZaurr -Type GPOList -Exclusions {
     Skip-GroupPolicy -Name 'All | Trusted Websites' -DomaiName 'ad.evotec.xyz'
     '{D39BF08A-87BF-4662-BFA0-E56240EBD5A2}'
+    "104da6a7-c7d2-48da-b24b-8fa584f7b0b6"
+    "{087b4f69-c541-429f-8dfd-0eb3ed133910}"
     'COMPUTERS | Enable Sets'
-}
+    '24194523-bb82-439c-a533-abf4f30fa2c4'
+    '{31b2f340-016d-11d2-945f-00c04fb984f9 } '
+} -PassThru
+
+$Output.GPOList
