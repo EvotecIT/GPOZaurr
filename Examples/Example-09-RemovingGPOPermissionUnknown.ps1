060fb95064d6be309ffc3d1a907d5219dedce12a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-17 07:30:17 +0200

    Update example

--- Examples/Example-09-RemovingGPOPermissionUnknown.ps1
+++ Examples/Example-09-RemovingGPOPermissionUnknown.ps1
@@ -1,3 +1,3 @@
 ﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
 #Clear-Host
-Remove-GPOZaurrPermission -Verbose -Type Unknown -LimitProcessing 1 #-WhatIf
+Remove-GPOZaurrPermission -Verbose -Type Unknown -LimitProcessing 5 -GPOName 'CA TEST'
