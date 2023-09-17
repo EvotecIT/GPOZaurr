fdfc700c6f1426e741fa41ba072474222bffb272
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-17 07:30:03 +0200

    Update example

--- Examples/Example-02-RemoveEmptyAndUnlinkedGPOs.ps1
+++ Examples/Example-02-RemoveEmptyAndUnlinkedGPOs.ps1
@@ -1,7 +1,11 @@
 ﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
 
-# Remove GPOS
-Remove-GPOZaurr -Type Empty, Unlinked -BackupPath "$Env:UserProfile\Desktop\GPO" -BackupDated -LimitProcessing 2 -Verbose -WhatIf
+# Remove GPOS, but don't touch 2 defined exclusions
+$ExcludeGroupPolicies = {
+    'TEST | Drive Mapping 1',
+    'TEST | Drive Mapping 2'
+}
+Remove-GPOZaurr -Type Empty -BackupPath "$Env:UserProfile\Desktop\GPO" -BackupDated -LimitProcessing 3 -Verbose -WhatIf -ExcludeGroupPolicies $ExcludeGroupPolicies
 
 # Remove GPOS, but don't touch 2 defined exclusions
 Remove-GPOZaurr -Type Empty, Unlinked -BackupPath "$Env:UserProfile\Desktop\GPO" -BackupDated -LimitProcessing 2 -Verbose -WhatIf {
