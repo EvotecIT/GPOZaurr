edcba71e7f69dd81d0c939f34d667fcdd7bc4ebe
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-04 11:34:29 +0100

    examples added

---
+++ Examples/Example-56-AnalyseGPOs.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
+
+$Output = Invoke-GPOZaurrContent -Verbose -GPOName 'Default Domain Policy'
+$Output | Format-Table
+
+<#
+# You need PSWriteOffice for that
+foreach ($Key in $Output.Keys) {
+    $Output[$Key] | Export-OfficeExcel -FilePath $Env:USERPROFILE\Desktop\GPOAnalysis.xlsx -WorkSheetName $Key
+}
+
+Invoke-GPOZaurr -Type GPOAnalysis -GPOName 'ALL | Allow use of biometrics', 'ALL | Enable RDP' -GPOGUID '{31B2F340-016D-11D2-945F-00C04FB984F9}' -IncludeDomains 'ad.evotec.xyz' -Verbose
+
+#>
---
+++ Examples/Example-57-FindGPOWithMissingFiles.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
+
+# search by guid or name for specific GPOs
+#Get-GPOZaurrMissingFiles -GPOGUID '{2F326111-C21B-4892-B7BC-9BDCB201FFCC}' | Format-Table
+
+# search for all GPOs with missing files
+#Get-GPOZaurrMissingFiles -BrokenOnly | Format-Table
+
+# search for all GPOs with missing files for everythin
+Invoke-GPOZaurr -Type GPOBrokenPartially #, GPOBroken, GPOBrokenLink
