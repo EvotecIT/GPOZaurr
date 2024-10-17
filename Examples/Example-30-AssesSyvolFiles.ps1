790b3b97e0b1349ae751bce9bea6b914330f20fc
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-10-17 22:30:41 +0200

    update example

--- Examples/Example-30-AssesSyvolFiles.ps1
+++ Examples/Example-30-AssesSyvolFiles.ps1
@@ -1,4 +1,5 @@
 Import-Module "$PSScriptRoot\..\GPoZaurr.psd1" -Force
 
-$Files = Get-GPOZaurrFiles -Limited -Signature
-$Files | ConvertTo-Excel -OpenWorkBook -FilePath $Env:USERPROFILE\Desktop\GPOTesting.xlsx -ExcelWorkSheetName 'GPO Output' -AutoFilter -AutoFit -FreezeTopRowFirstColumn
+$Files = Get-GPOZaurrFiles -Type All -Verbose
+#$Files | ConvertTo-Excel -OpenWorkBook -FilePath $Env:USERPROFILE\Desktop\GPOTesting.xlsx -ExcelWorkSheetName 'GPO Output' -AutoFilter -AutoFit -FreezeTopRowFirstColumn
+$Files | Format-Table
